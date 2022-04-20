c334286fc6ea4d686ba6537d212e89773038a15b
senpai-10 <bmjfdrh@gmail.com>
2022-04-20 09:03:15 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", fibonacci_sequence(50));
+}
+
+fn fibonacci_sequence(n: u8) -> u64 {
+    let mut prev_num: u64 = 0;
+    let mut curr_num: u64 = 1;
+
+    for _ in 1..n {
+        let next = prev_num + curr_num;
+        prev_num = curr_num;
+        curr_num = next;
+    }
+
+    return curr_num;
 }
